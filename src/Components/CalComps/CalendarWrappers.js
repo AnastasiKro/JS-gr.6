455f1ee151f7555261e952eb14c53306dfd1a4cb
yaAvtobus <naskrop@gmail.com>
2022-11-26 14:58:15 +0300

    Added Calendar cell

---
+++ src/Components/CalComps/CalendarWrappers.js
@@ -0,0 +1,74 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+export const ShadowWrapper = styled.div`
+border-top: 1px solid #737374;
+border-left: 1px solid #464648;
+border-right: 1px solid #464648;
+border-bottom: 1px solid #464648;
+border-radius: 8px;
+overflow: hidden;
+box-shadow: 0 0 0 1px #1A1A1A, 0 8px 20px 6px #888;
+`;
+
+export const DivWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  //background-color: #1F1F21;
+  //color:
+  padding: 16px;
+`;
+export const TextWrapper = styled.span`
+  font-size: 32px;
+`;
+export const TitleWrapper = styled(TextWrapper)`
+  font-weight: bold;
+  margin-right: 8px;
+`;
+
+export const ButtonWrapper = styled('button')`
+  border: unset;
+  background-color: #E6E6FA;
+  height: 32px;
+  margin-right: 2px;
+  border-radius: 4px;
+  border-color: #7B68EE;
+  border-style: solid;
+
+`;
+
+export const GridWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+  grid-template-rows: repeat(6, 1fr);
+  grid-gap: 1.2px;
+  background-color: #404040;
+  `;
+
+export const CellWrapper = styled.div`
+  min-width: 140px;
+  min-height: 80px;
+background-color: ${props=> props.isWeekend ? '#FFA07A' : '#E6E6FA'};
+  //color: #444
+`;
+export const CellRow = styled.div`
+  display: flex;
+  justify-content: ${props => props.justifyContent ? props.justifyContent: 'flex-start'};
+`;
+export const DayWrapper = styled.div`
+  height: 33px;
+  width: 33px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 2px;
+`;
+export const CurrentDayWrapper = styled.div`
+  height: 100%;
+  width: 100%;
+  background: #f00;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
--- src/routes/Calendar.js
+++ src/routes/Calendar.js
@@ -1,10 +1,64 @@
-import React from 'react';
+import * as React from 'react';
+import {ShadowWrapper, DivWrapper, ButtonWrapper, PreviousMonthWrapper, TextWrapper, GridWrapper, CellWrapper, CellRow, DayWrapper, CurrentDayWrapper, TitleWrapper} from '../Components/CalComps/CalendarWrappers';
+import moment from 'moment';
+import DatesPicker from '../Components/CalComps/DatesPicker';
 
-export default function Calendar () {
-    return(
-        <div>
-            <h1>
-                ТЕСТ КАЛЕНДАРЬ
-            </h1>
-        </div>
-        )}
+
+
+export default function Calendar() {
+  window.moment = moment;
+  moment.updateLocale('en', {week: {dow: 1}})
+  //const [value, setValue] = React.useState(dayjs('2022-04-07'));
+  const totalDays = 42;
+  const today = moment();
+  const startDay = today.clone().startOf('month').startOf('week');
+  const endDay = moment().endOf('month').endOf('week');
+  const calendar = [];
+  const day = startDay.clone();
+  const month = today.clone();
+  day.subtract(1, 'day');
+  const isCurrentMonth = (day) => moment().isSame(day, 'month'); 
+  const isCurrentDay = (day) => moment().isSame(day, 'day'); 
+  const daysArray = [...Array(42)].map(()=> day.add(1, 'day').clone());
+  const MonthYear = ()=>(
+    <DivWrapper>
+      <DivWrapper>
+      <ButtonWrapper id="prev" onClick={()=>today.subtract(1, 'month')}> &lt; </ButtonWrapper>
+    </DivWrapper>
+    <DivWrapper>
+        <TitleWrapper>{today.format('MMMM')}</TitleWrapper>
+        <TextWrapper>{today.format('YYYY')}</TextWrapper>
+      </DivWrapper>
+      <DivWrapper>
+        <ButtonWrapper> &gt; </ButtonWrapper>
+      </DivWrapper>
+    </DivWrapper>
+  )
+
+  return (
+  <div>
+    <div>previous next</div>
+    <DatesPicker />
+    <MonthYear/>
+    <ShadowWrapper>
+   <GridWrapper>
+     {
+       daysArray.map((dayItem)=>(
+         <CellWrapper 
+         key={dayItem.format('DDMMYYYY')}
+         isWeekend={dayItem.day()===6 || dayItem.day()===0}>
+         <CellRow justifyContent={'flex-end'}>
+         <DayWrapper>
+           {!isCurrentDay(dayItem) && isCurrentMonth && dayItem.format('DD')}
+           {!isCurrentDay(dayItem) && !isCurrentMonth &&<PreviousMonthWrapper> {dayItem.format('DD')}</PreviousMonthWrapper>}
+           {isCurrentDay(dayItem) && <CurrentDayWrapper>{dayItem.format('DD')} </CurrentDayWrapper>}
+           </DayWrapper>
+           </CellRow>
+         </CellWrapper>
+       ))
+     }
+   </GridWrapper>
+   </ShadowWrapper>
+   </div>
+  );
+}
