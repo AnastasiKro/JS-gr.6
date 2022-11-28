cf061f065cab0b2c165450a576a2ccefa1aa1793
Anastasiia <62606871+yaAvtobus@users.noreply.github.com>
2022-11-28 10:38:50 +0300

    Update Calendar.js

--- src/routes/Calendar.js
+++ src/routes/Calendar.js
@@ -2,26 +2,28 @@ import React from 'react';
 import dayjs from 'dayjs';
 import DatesPicker from '../Components/CalComps/DatesPicker';
 import MonthChanger from '../Components/CalComps/MonthChanger';
-import { ShadowWrapper, DivWrapper, ButtonWrapper, PreviousMonthWrapper, TextWrapper, GridWrapper, CellWrapper, CellRow, DayWrapper, CurrentDayWrapper, TitleWrapper} from '../Components/CalComps/CalendarWrappers';
+import { ShadowWrapper, DivWrapper, DayOfWeekWrapper, ButtonWrapper, PreviousMonthWrapper, GridWrapper, CellWrapper, CellRow, DayWrapper, CurrentDayWrapper} from '../Components/CalComps/CalendarWrappers';
 import moment from 'moment';
+import Cell from '../Components/CalComps/Cell';
 
 export default function Calendar() {
   window.moment = moment;
   moment.updateLocale('en', {week: {dow: 1}})
   
-  const totalDays = 42;
-  const today0 = moment();
+  //const totalDays = 42;
+  //const today0 = moment();
   const [today, setToday] = React.useState(moment());
   const startDay = today.clone().startOf('month').startOf('week');
-  console.log(startDay);
-  const endDay = moment().endOf('month').endOf('week');
+  //console.log(startDay);
+  //const endDay = moment().endOf('month').endOf('week');
   const day = startDay.clone();
   day.subtract(1, 'day');
-  const isCurrentMonth = (day) => today.isSame(day, 'month');
-  const isCurrentDay = (day) => today.isSame(day, 'day'); 
-  const daysArray = [...Array(42)].map(()=> day.add(1, 'day').clone());
+  //const isCurrentMonth = (day) => today.isSame(day, 'month');
+  //const isCurrentDay = (day) => moment().isSame(day, 'day'); 
+  //const daysArray = [...Array(42)].map(()=> day.add(1, 'day').clone());
   const handleDayChange = (newDay) => {
     console.log(newDay)
+    setToday(newDay);
   }
   const handleMonthChange = (newDay) => {
     setToday(newDay)
@@ -31,30 +33,8 @@ export default function Calendar() {
     <DatesPicker ToDay={today} onWork={handleDayChange}/>
     <MonthChanger today={today} onWork={handleMonthChange}/>
     <ShadowWrapper>
-   <GridWrapper>
-     <CellWrapper isHeader> Пн </CellWrapper>
-     <CellWrapper isHeader> </CellWrapper>
-     <CellWrapper isHeader> </CellWrapper>
-     <CellWrapper isHeader> </CellWrapper>
-     <CellWrapper isHeader> </CellWrapper>
-     <CellWrapper isHeader> </CellWrapper>
-     <CellWrapper isHeader> </CellWrapper>
-     {
-       daysArray.map((dayItem)=>(
-         <CellWrapper 
-         key={dayItem.format('DDMMYYYY')}
-         isWeekend={dayItem.day()===6 || dayItem.day()===0}>
-         <CellRow justifyContent={'flex-end'}>
-         <DayWrapper>
-           {!isCurrentDay(dayItem) && isCurrentMonth(dayItem) && dayItem.format('DD')}
-           {!isCurrentDay(dayItem) && !isCurrentMonth(dayItem) && <PreviousMonthWrapper> {dayItem.format('DD')}</PreviousMonthWrapper>}
-           {isCurrentDay(dayItem) && <CurrentDayWrapper>{dayItem.format('DD')} </CurrentDayWrapper>}
-           </DayWrapper>
-           </CellRow>
-         </CellWrapper>
-       ))
-     }
-   </GridWrapper>
+      <Cell today={today}>
+      </Cell>
    </ShadowWrapper>
    </div>
   );
